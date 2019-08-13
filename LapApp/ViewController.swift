104a49ec858015cf26a8fb54c48a350e26673e83
KazakovaNet <kazakovaNet@yandex.ru>
2019-08-13 22:04:08 +0300

    first commit

---
+++ LapApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LapApp
+//
+//  Created by Natalia Kazakova on 13/08/2019.
+//  Copyright © 2019 Natalia Kazakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
