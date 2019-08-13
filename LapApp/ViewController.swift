2fc1246ca7eed091ebf410791711d316e37c8a93
KazakovaNet <kazakovaNet@yandex.ru>
2019-08-13 22:12:30 +0300

    Настроен UI

--- LapApp/ViewController.swift
+++ LapApp/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate let cellIdentifier = "lapCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+//MARK: - Actions
+/***************************************************************/
+
+extension ViewController {
+    @IBAction func addLap(_ sender: UIBarButtonItem) {
+    }
+}
+
+
+//MARK: - UITableViewDataSource
+/***************************************************************/
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
